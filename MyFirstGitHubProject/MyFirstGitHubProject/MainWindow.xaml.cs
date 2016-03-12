9d597db2d72a9203130cacc2f7599e3889747059
Yee Jia Xing <jia_87@hotmail.com>
2016-03-12 10:06:50 +0800

    Add HelloWorld Button

--- MyFirstGitHubProject/MyFirstGitHubProject/MainWindow.xaml.cs
+++ MyFirstGitHubProject/MyFirstGitHubProject/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MyFirstGitHubProject
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
